eb2cfb22ab5040a83fae64ffdde6f78e1bf243b3
zonexports <zonexports2@gmail.com>
2026-09-07 16:41:48 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAgsICU7QM-0A6NqJaF0n0OLyU8Lx9JwIE",
+  authDomain: "zonexports-95c17.firebaseapp.com",
+  databaseURL: "https://zonexports-95c17-default-rtdb.firebaseio.com",
+  projectId: "zonexports-95c17",
+  storageBucket: "zonexports-95c17.firebasestorage.app",
+  messagingSenderId: "564782892677",
+  appId: "1:564782892677:web:33eaa423957eba5aa5cbec"
+};
